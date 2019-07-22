5e7ced7d48024e6be40aa8609f194746e0abccdd
Sebastian Skuse <seb@apphq.co.uk>
2019-07-22 13:50:10 +0100

    Add tests for a CommandHandler

---
+++ Tests/DiscourseTests/CommandHandlerTests.swift
@@ -0,0 +1,29 @@
+//
+//  CommandHandlerTests.swift
+//  DiscourseTests
+//
+//  Created by Seb Skuse on 22/07/2019.
+//
+
+import XCTest
+@testable import Discourse
+
+class CommandHandlerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testWhenThereIsNoHandlerRegisteredForAGivenVerbAnAppropriateErrorMessageIsReturned() {
+        
+    }
+    
+    func testWhenThereIsAHandlerRegisteredButTheArgumentsAreInvalidAnErrorIsReturned() {
+        
+    }
+
+}

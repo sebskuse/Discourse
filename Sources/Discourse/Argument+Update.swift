13efe49b731c7170a7dc7bcc2d9356a37f3bfca6
Sebastian Skuse <seb@apphq.co.uk>
2019-07-22 13:40:08 +0100

    Add argument parsing

---
+++ Sources/Discourse/Argument+Update.swift
@@ -0,0 +1,39 @@
+//
+//  Argument+Update.swift
+//  Discourse
+//
+//  Created by Seb Skuse on 22/07/2019.
+//
+
+import Foundation
+
+public struct ArgumentUpdate<T: ArgumentStringConvertible> {
+    public let name: String
+    public let value: T
+    
+    public init(name: String, value: T) {
+        self.name = name
+        self.value = value
+    }
+}
+
+public extension Argument {
+    
+    /// Produces an `ArgumentUpdate`, representing a potential update to
+    /// and `Argument`s value based on new values coming from `pairs`.
+    /// - Parameter pairs: A dictionary of new arguments
+    /// An error will be thrown if the argument type fails to parse, or if
+    /// there is no argument present.
+    func argumentValue<T: ArgumentStringConvertible>(from pairs: [String: String]) throws -> ArgumentUpdate<T> {
+        if let valueUsingPrimaryKey = pairs[name] {
+            return ArgumentUpdate(name: name, value: try T.parse(argumentString: valueUsingPrimaryKey))
+        }
+        if let secondaryKeys = aliases,
+            let firstValidAlias = secondaryKeys.first(where: { pairs[$0] != nil }),
+            let value = pairs[firstValidAlias] {
+            return ArgumentUpdate(name: firstValidAlias, value: try T.parse(argumentString: value))
+        }
+        
+        throw ArgumentValueUpdateError.argumentNotFound
+    }
+}
---
+++ Sources/Discourse/ArgumentStringConvertible.swift
@@ -0,0 +1,52 @@
+//
+//  ArgumentStringConvertible.swift
+//  Discourse
+//
+//  Created by Seb Skuse on 22/07/2019.
+//
+
+import Foundation
+
+/// A type that can be converted to from a command line argument.
+public protocol ArgumentStringConvertible {
+    
+    /// Parses the given argument value string to `Self`.
+    static func parse(argumentString: String) throws -> Self
+}
+
+// MARK: Implementations
+
+extension String: ArgumentStringConvertible {
+    public static func parse(argumentString: String) throws -> String {
+        return argumentString
+    }
+}
+
+extension URL: ArgumentStringConvertible {
+    public static func parse(argumentString: String) throws -> URL {
+        return URL(fileURLWithPath: argumentString).standardizedFileURL
+    }
+}
+
+extension Bool: ArgumentStringConvertible {
+    public static func parse(argumentString: String) throws -> Bool {
+        guard let result = Bool(argumentString) else {
+            throw InvalidArgumentError(message: "\(argumentString) does not represent a boolean")
+        }
+        return result
+    }
+}
+
+extension Optional where Wrapped: ArgumentStringConvertible {
+    public static func parse(argumentString: String) throws -> Wrapped {
+        return try Wrapped.parse(argumentString: argumentString)
+    }
+}
+
+struct InvalidArgumentError: LocalizedError {
+    let message: String
+
+    var errorDescription: String? {
+        return message
+    }
+}
